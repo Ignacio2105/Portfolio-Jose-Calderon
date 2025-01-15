71cd03a8f94e57bcf2339b6c6d90ca1de833282d
Ignacio C <ignacio.caldsil@gmail.com>
2025-01-14 19:10:27 -0500

    actualizacion de portafolio con nuevos proyectos

--- src/redes_trabajos.js
+++ src/redes_trabajos.js
@@ -14,6 +14,24 @@ const TRABAJOS = [
     website: 'https://ignacio2105.github.io/Landing-Page/public/',
     github: 'https://github.com/Ignacio2105/Landing-Page',
     imagen: './assets/proyectos/Proyecto-Landing_Page.png'
+  },
+  {
+    nombre: 'ProgramaYa',
+    fecha: '30 - Nov - 2024',
+    descripcion:
+      'Página web estática para aprendizaje en online en programación.',
+      website: 'https://ignacio2105.github.io/Portafolio/',
+      github: 'https://github.com/Ignacio2105/Portafolio',
+      imagen: '../assets/proyectos/Proyecto-ProgramaWeb.png'
+  },
+  {
+    nombre: 'BataBit',
+    fecha: '20 - Dic - 2024',
+    descripcion:
+    'Página web para una empresa real relacionado al uso de las criptomonedas en Latam.',
+    website: 'https://ignacio2105.github.io/Batabit/',
+    github: 'https://github.com/Ignacio2105/Batabit',
+    imagen: '../assets/proyectos/Proyecto-BataBit.jpg'
   }
 ]
 
